dbd3750980cabe9d38643dae8f5979bc695b286a
BeeKnowThex <bbiswakarma996@gmail.com>
2025-02-03 20:10:29 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('changeMessageBtn').addEventListener('click', function() {
+    document.getElementById('message').textContent = 'You clicked the button! Have a great day!';
+});
